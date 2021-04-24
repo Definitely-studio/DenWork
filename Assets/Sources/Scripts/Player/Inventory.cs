d69330a43904f30629d7dbed3aebba971a981f65
romangulbis <43339065+romangulbis@users.noreply.github.com>
2021-04-24 12:56:03 +0300

    Aboba

--- Assets/Sources/Scripts/Player/Inventory.cs
+++ Assets/Sources/Scripts/Player/Inventory.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public List<string> playerInventory = new List<string>() {};
-
+	public GameObject inventoryUI;
+	private bool isInventoryActive = true;
 	void Start()
 	{
 		playerInventory.Add("Testname");
 	}
+
+	private void Update()
+	{
+	    if( Input.GetKeyDown( KeyCode.I ) && isInventoryActive )  {
+	    inventoryUI.SetActive(false);
+	    isInventoryActive = false;
+	}
+}
+
 }
