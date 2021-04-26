0d045279257b8ec9005b9609af281ad9c03966b7
romangulbis <43339065+romangulbis@users.noreply.github.com>
2021-04-26 20:09:46 +0300

    update player and zombies animation

--- Assets/Sources/Scripts/Player/Player.cs
+++ Assets/Sources/Scripts/Player/Player.cs
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
     public Animator b_Animator;
     public bool facingRight = true;
 
+    public GameObject body;
     public GameObject f_body;
     public GameObject b_body;
 
@@ -37,6 +38,7 @@ public class Player : MonoBehaviour
     }
     private void Start()
     {
+        b_body.SetActive(false);
         SetHP(MaxHP);
         Debug.Log(currentHP);
     }
@@ -68,7 +70,8 @@ public class Player : MonoBehaviour
     {
          if (_gun != null){
         _gun.Shoot();
-
+            f_Animator.SetTrigger("Shot");   
+            b_Animator.SetTrigger("Shot");                    
         }    
     }
 
@@ -79,56 +82,61 @@ public class Player : MonoBehaviour
          
     Vector2 AimPosition = _input.Player.MousePosition.ReadValue<Vector2>();
        if(_gun != null)
+       {
             _gun.SetAimPoint(AimPosition);
+        }           
             
     }
     private void FixedUpdate()
     {
         moveDirection = _input.Player.Move.ReadValue<Vector2>();
+        Vector2 mousePosition = _input.Player.MousePosition.ReadValue<Vector2>();
         //Debug.Log(moveDirection);
 
         Movement(moveDirection);
-        Rotation(moveDirection);
+
+        Rotation(mousePosition, moveDirection);        
         
     }
 
     private void Movement(Vector2 move)
     {
-        //move = move.normalized;
         Vector2 dir = _rigidbody.position + move * (Time.fixedDeltaTime * _velocity);
-        _rigidbody.transform.position = dir ;     
+        _rigidbody.transform.position = dir ;  
+  
         
        
     }
 
-    private void Rotation(Vector2 dir) {
-        float move = dir.x;
+    private void Rotation(Vector2 MouseDir, Vector2 moveDir) {
+        Vector3 worldPosMouse = Camera.main.ScreenToWorldPoint(MouseDir);
+        Vector3 lookDir = ( new Vector3(_rigidbody.position.x, _rigidbody.position.y ,0) - worldPosMouse);
 
-        if (move < 0 && facingRight) {
-            transform.Rotate(Vector3.up * 180);
-            facingRight = !facingRight;
-            f_Animator.SetBool("isWalk", true);
-            b_Animator.SetBool("isWalk", true);            
+        if (lookDir.x < 0) {
+            body.transform.eulerAngles = new Vector3(0f, 0f, 0f);          
         }
-        if (move > 0 && !facingRight) {
-            transform.Rotate(Vector3.up * 180);
-            facingRight = !facingRight;
-            f_Animator.SetBool("isWalk", true);
-            b_Animator.SetBool("isWalk", true);            
+        if (lookDir.x > 0) {
+            body.transform.eulerAngles = new Vector3(0f, 180f, 0f);             
         }
-        if (move == 0 && dir.y == 0) {
+        if (lookDir.y > 0) {
+            f_body.SetActive(false);
+            b_body.SetActive(true);           
+        }
+        if (lookDir.y < 0) {
+            f_body.SetActive(true);
+            b_body.SetActive(false);           
+        }          
+
+
+        if (moveDir.magnitude > 0) {
+            f_Animator.SetBool("isWalk", true);
+            b_Animator.SetBool("isWalk", true);              
+        }
+
+        if (moveDir.magnitude == 0) {
             f_Animator.SetBool("isWalk", false);
             b_Animator.SetBool("isWalk", false);               
-        }
-
-        if (dir.y > 0) {
-            f_body.SetActive(false);
-            b_body.SetActive(true);              
-        }        
-        if (dir.y <= 0) {
-            f_body.SetActive(true);
-            b_body.SetActive(false);              
-        }      
+        }     
 
     }
 
